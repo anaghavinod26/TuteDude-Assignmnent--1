1e3658844c69aa88ca9e9a546677c9e05b7e5fa9
anaghavinod26 <anaghavinod26@gmail.com>
2025-06-06 23:36:20 +0530

    Assignment1 Task2.py

--- Assignment1 Task2.py
+++ Assignment1 Task2.py
@@ -1,4 +1,4 @@
 a1 = input("Input your first name: ")
 a2 = input("Input your last name: ")
-print("\nHello,",a1,a2,"! Welcome to the Python program")
+print("\nHello, "+ a1 + ' ' + a2 + "! Welcome to the Python program")
 
